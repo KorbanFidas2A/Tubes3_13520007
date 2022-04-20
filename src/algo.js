8598f6ed230c0b91b7fb225bb0f5c9105e5c272a
roastland <tfpruslanali@gmail.com>
2022-04-21 04:57:14 +0700

    shortened sanitasi input

--- src/algo.js
+++ src/algo.js
@@ -1,14 +1,6 @@
-let text = "AGCTU";
-let noSpaces = /^\S*$/;
-let noLowCase = /^[^a-z]*$/;
-let onlyAGCT = /^[AGCT]*$/;
+let text = "AAAAAGGGCCCTTT";
+let valid = /^[AGCT]*$/;
 
-let result1 = noSpaces.test(text);
-let result2 = noLowCase.test(text);
-let result3 = onlyAGCT.test(text);
-let result = result1 && result2 && result3;
+let result = valid.test(text);
 
-console.log(result1)
-console.log(result2)
-console.log(result3)
 console.log(result)
