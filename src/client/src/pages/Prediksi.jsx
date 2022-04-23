19ba7450f1ddfa3b27e130a38eeee7e097126fe5
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2022-04-23 20:05:49 +0700

    add states on Prediksi page

--- src/client/src/pages/Prediksi.jsx
+++ src/client/src/pages/Prediksi.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button } from "../components";
 import { images } from "../constants";
 
@@ -12,13 +12,57 @@ const Prediksi = () => {
 
   const [name, setName] = useState("Belum Terisi");
   const [penyakit, setPenyakit] = useState(listPenyakit[0]);
+  const [filename, setFilename] = useState("Tidak ada berkas yang dipilih");
+  const fileInputRef = useRef(null);
+  const [isFileValid, setIsFileValid] = useState(false);
+  const [isAllFilled, setIsAllFilled] = useState(true);
+
+  var DNA = "";
   const today = new Date();
+
   const date =
     String(today.getDate()).padStart(2, "0") +
     "-" +
     String(today.getMonth() + 1).padStart(2, "0") +
     "-" +
     today.getFullYear();
+
+  const handleUploadFileButton = (e) => {
+    e.preventDefault();
+    fileInputRef.current.click();
+  };
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    setFilename(file.name);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      DNA = e.target.result;
+      fileValidation();
+    };
+    reader.readAsText(file);
+  };
+
+  const fileValidation = () => {
+    const regex = /^[AGCT]*$/;
+    if (regex.test(DNA)) {
+      setIsFileValid(true);
+    } else {
+      setIsFileValid(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    {
+      name === "Belum Terisi" || penyakit === listPenyakit[0] || !isFileValid
+        ? setIsAllFilled(false)
+        : setIsAllFilled(true);
+    }
+  };
+
+  const warningMessage = () => {};
 
   return (
     <div className="relative flex flex-col px-[1.75rem] pt-[4.5rem] pb-[3rem] lg:flex-row lg:px-[9.75rem] lg:pt-[10rem] lg:pb-[8.5rem]">
@@ -28,7 +72,6 @@ const Prediksi = () => {
           Tes DNA-mu!
         </h1>
         <form>
-          
           {/* NAME */}
           <div className="mb-[1.5rem] lg:mb-[3rem]">
             <p className="mb-[1rem] text-[1rem] font-medium lg:mb-[1.5rem] lg:text-[1.5rem]">
@@ -48,19 +91,35 @@ const Prediksi = () => {
             <p className="mb-[1rem] text-[1rem] font-medium lg:mb-[1.5rem] lg:text-[1.5rem]">
               Berkas DNA
             </p>
-            <div className="mb-[0.75rem] text-[0.668rem] font-medium text-darkgrey lg:mb-[1.125rem] lg:text-[1rem]">
+            <div
+              className={
+                `mb-[0.75rem] text-[0.668rem] font-medium text-darkgrey lg:mb-[1.125rem] lg:text-[1rem] ` +
+                (isFileValid || filename === "Tidak ada berkas yang dipilih"
+                  ? `text-darkgrey`
+                  : `text-red`)
+              }
+            >
               <p>*Berkas yang diunggah harus berekstensi .txt</p>
               <p>*Berkas hanya berisi huruf A, C, G, dan/atau T</p>
               <p>*Tidak boleh ada enter, spasi, dan huruf kecil</p>
             </div>
             <p className="text-[0.667rem] font-medium lg:text-[1rem]">
               <Button
-                className={`mr-[1rem] bg-turquoise p-[0.6rem] text-[0.667rem] font-medium hover:bg-orange lg:mr-[1.5rem] lg:p-[1rem] lg:text-[1rem]`}
+                className={`!mr-[1rem] !bg-turquoise !p-[0.6rem] !text-[0.667rem] !font-medium hover:!bg-orange lg:!mr-[1.5rem] lg:!p-[1rem] lg:!text-[1rem]`}
+                onClick={handleUploadFileButton}
               >
                 Unggah Berkas
-              </Button>{" "}
-              Tidak ada berkas yang dipilih
+              </Button>
+              {filename}
             </p>
+            <input
+              type="file"
+              className="hidden"
+              multiple={false}
+              accept=".txt,.text,text/plain"
+              onChange={handleFileUpload}
+              ref={fileInputRef}
+            />
           </div>
 
           {/* PENYAKIT/KELAINAN */}
@@ -85,7 +144,16 @@ const Prediksi = () => {
             </select>
           </div>
 
-          <Button>Lihat Hasil</Button>
+          <Button className={`mb-[1rem]`} onClick={handleSubmit}>
+            Lihat Hasil
+          </Button>
+          <p
+            className={
+              `text-[0.667rem] lg:text-[1rem] font-medium text-red ` + (isAllFilled ? `hidden` : `block`)
+            }
+          >
+            *Tidak boleh ada yang kosong
+          </p>
         </form>
       </div>
 
@@ -126,7 +194,11 @@ const Prediksi = () => {
               Hasil
             </p>
             <p className="basis-1/2 text-[1rem] text-darkgrey lg:text-[1.5rem]">
-              <i>Belum Terisi</i>
+              <i>
+                {name === "" || name === "Belum Terisi"
+                  ? "Belum Terisi"
+                  : "Tekan tombol Lihat Hasil untuk melihat hasil"}
+              </i>
             </p>
           </div>
         </div>
