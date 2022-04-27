7c053d1c4c8b040fbd0cae4c70511134d9f20521
roastland <tfpruslanali@gmail.com>
2022-04-28 02:48:16 +0700

    add LCS &  similarity test

--- src/algo.js
+++ src/algo.js
@@ -5,10 +5,15 @@ let result = valid.test(input);
 
 console.log(result)
 
-let toMatch = "GCCCT";
+let toMatch = "AGGGCCCTTC";
 
 console.log(kmpMatch(input, toMatch))
 console.log(bmMatch(input, toMatch))
+let a = "sitting";
+let b = "kitten";
+
+console.log(levenshteinDist(input, toMatch))
+console.log(similarityTest(input, toMatch))
 
 // KMP algorithm, return index di mana pattern ditemukan jika pattern
 // ditemukan pada text, return -1 jika pattern tidak ditemukan
@@ -34,7 +39,7 @@ function kmpMatch(text, pattern) {
             i++;
         } 
     }
-
+    
     return -1;
 }
 
@@ -127,3 +132,67 @@ function hammingDistance(text1, text2) {
     
     return dist;
  };
+
+
+
+function levenshteinDist(text, pattern) {
+    let m = text.length;
+    let n = pattern.length;
+
+    const distance = Array(n + 1).fill(null).map(() => Array(m + 1).fill(null));
+    
+    for (let i = 0; i <= m; i++) {
+        distance[0][i] = i;
+    }
+
+    for (let j = 0; j <= n; j++) {
+        distance[j][0] = j;
+    }
+
+    var subsCost;
+    for (let j = 1; j <= n; j++) {
+        for (let i = 1; i <= m; i++) {
+            if (pattern.charAt(j-1) == text.charAt(i-1)) {
+                subsCost = 0;
+            } else {
+                subsCost = 1;
+            }
+            //console.log('delete:', distance[j][i-1] + 1)
+            //console.log('insert:',distance[j-1][i] + 1)
+            //console.log('substitute:', distance[j-1][i-1] + subsCost)
+            distance[j][i] = Math.min(distance[j][i-1] + 1,             // deletion
+                                      distance[j-1][i] + 1,             // insertion
+                                      distance[j-1][i-1] + subsCost);   // substitution
+        }
+    }
+
+    return distance[n][m];
+}
+
+// Longest Common Subsequence, menghitung jumlah karakter yang sama
+// pada pola yang sama (sequential tapi tidak harus contigous) 
+function LCS(text, pattern) {
+    let m = text.length;
+    let n = pattern.length;
+    const L = Array(m + 1).fill(null).map(() => Array(n + 1).fill(null));
+    
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i == 0 || j == 0) {
+                L[i][j] = 0;
+            } else if (pattern.charAt(j-1) == text.charAt(i-1)) {
+                L[i][j] = L[i-1][j-1] + 1;
+            } else {
+                L[i][j] = Math.max(L[i-1][j], L[i][j-1]);
+            }
+        }
+    }
+
+    return L[m][n];    
+}
+
+// menghitung persentase kemiripan
+function similarityTest(text, pattern) {
+    let charSimilar = LCS(text, pattern);
+    return (charSimilar / pattern.length) * 100;
+}
