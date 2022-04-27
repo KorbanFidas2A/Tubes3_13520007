759e4d740ae386f36388235f242f696357a96e68
roastland <tfpruslanali@gmail.com>
2022-04-28 02:56:39 +0700

    move algo to backend

--- src/server/routes/algo.js
+++ src/server/routes/algo.js
@@ -1,9 +1,6 @@
 // KMP algorithm, return index di mana pattern ditemukan jika pattern
 // ditemukan pada text, return -1 jika pattern tidak ditemukan
 function kmpMatch(text, pattern) {
-    console.log("masuk kmp")
-    console.log(text)
-    console.log(pattern)
     let n = text.length;
     let m = pattern.length;
 
@@ -102,11 +99,42 @@ function buildLast(pattern) {
     return last;
 }
 
+// menghitung persentase kemiripan menggunakan LCS
+function similarityTest(text, pattern) {
+    let charSimilar = LCS(text, pattern);
+    return (charSimilar / pattern.length) * 100;
+}
+
+// Longest Common Subsequence, menghitung jumlah karakter yang sama
+// pada pola yang sama (sequential tapi tidak harus contigous) 
+function LCS(text, pattern) {
+    let m = text.length;
+    let n = pattern.length;
+    const L = Array(m + 1).fill(null).map(() => Array(n + 1).fill(null));
+    
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i == 0 || j == 0) {
+                L[i][j] = 0;
+            } else if (pattern.charAt(j-1) == text.charAt(i-1)) {
+                L[i][j] = L[i-1][j-1] + 1;
+            } else {
+                L[i][j] = Math.max(L[i-1][j], L[i][j-1]);
+            }
+        }
+    }
+
+    return L[m][n];    
+}
+
 module.exports = {
     bmMatch: function(text, pattern) {
         return bmMatch(text, pattern);
     },
     kmpMatch: function(text, pattern) {
         return kmpMatch(text, pattern);
+    },
+    similarityTest: function(text, pattern) {
+        return similarityTest(text, pattern);
     }
 };
