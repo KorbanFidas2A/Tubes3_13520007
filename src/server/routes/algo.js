95236cfb7f89173c8ff571f123ef5051054c78ab
roastland <tfpruslanali@gmail.com>
2022-04-29 22:57:50 +0700

    change lcs to levenshtein & fix similarity test

--- src/server/routes/algo.js
+++ src/server/routes/algo.js
@@ -137,33 +137,44 @@ function buildLast(pattern) {
 
 // menghitung persentase kemiripan menggunakan LCS
 function similarityTest(text, pattern) {
-  let charSimilar = LCS(text, pattern);
-  return (charSimilar / pattern.length) * 100;
+  let lvsdist = LCS(text, pattern);
+  return ((text.length - lvsdist) / text.length) * 100;
 }
 
-// Longest Common Subsequence, menghitung jumlah karakter yang sama
-// pada pola yang sama (sequential tapi tidak harus contigous)
-function LCS(text, pattern) {
+// Levenshtein Distance, menghitung jumlah perubahan karakter
+// pada pattern agar sesuai dengan text
+function levenshteinDist(text, pattern) {
   let m = text.length;
   let n = pattern.length;
-  const L = Array(m + 1)
-    .fill(null)
-    .map(() => Array(n + 1).fill(null));
 
+  const distance = Array(n + 1).fill(null).map(() => Array(m + 1).fill(null));
+  
   for (let i = 0; i <= m; i++) {
-    for (let j = 0; j <= n; j++) {
-      if (i == 0 || j == 0) {
-        L[i][j] = 0;
-      } else if (pattern.charAt(j - 1) == text.charAt(i - 1)) {
-        L[i][j] = L[i - 1][j - 1] + 1;
-      } else {
-        L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
-      }
-    }
+      distance[0][i] = i;
   }
 
-  return L[m][n];
+  for (let j = 0; j <= n; j++) {
+      distance[j][0] = j;
+  }
+
+  var subsCost;
+  for (let j = 1; j <= n; j++) {
+      for (let i = 1; i <= m; i++) {
+          if (pattern.charAt(j-1) == text.charAt(i-1)) {
+              subsCost = 0;
+          } else {
+              subsCost = 1;
+          }
+
+          distance[j][i] = Math.min(distance[j][i-1] + 1,             // deletion
+                                    distance[j-1][i] + 1,             // insertion
+                                    distance[j-1][i-1] + subsCost);   // substitution
+      }
+  }
+
+  return distance[n][m];
 }
+
 
 module.exports = {
   DNAValidation: function (DNA) {
